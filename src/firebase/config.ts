3eb83da145d50b160f003a97e69f80eaaca81882
NABANITA DEBNATH <dnnabanita@gmail.com>
2025-12-29 05:48:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8954641290-81428",
+  "appId": "1:94432253484:web:5776b7bb6f3238d9e3a389",
+  "apiKey": "AIzaSyAOkFzh4hrruj4FyF-IfcVJeuatZBc8Zkw",
+  "authDomain": "studio-8954641290-81428.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "94432253484"
+};
